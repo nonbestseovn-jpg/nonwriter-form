fffa47e362f2cb24a49fc8a25f5fa0fc5853d160
nonbestseovn-jpg <nonbestseo.vn@gmail.com>
2026-07-08 22:16:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL='DAN_LINK_APPS_SCRIPT_O_DAY';
+const SCRIPT_URL='https://script.google.com/macros/s/AKfycbzo_a5kwljUo_YaRJHYfBua55IinUgFBuwF0T6mheIPArzD6aIpyhHeYd8ubvOHxLyv/exec';
 document.getElementById('f').addEventListener('submit',async e=>{
 e.preventDefault();
 const data={name:name.value,phone:phone.value,website:website.value,service:service.value,message:message.value};
