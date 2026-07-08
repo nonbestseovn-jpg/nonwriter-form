9b0eba2500e5617e83dfe5fa0578a5956a2558a3
nonbestseovn-jpg <nonbestseo.vn@gmail.com>
2026-07-08 22:19:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.getElementById('f').addEventListener('submit',async e=>{
 e.preventDefault();
 const data={name:name.value,phone:phone.value,website:website.value,service:service.value,message:message.value};
 try{
-await fetch(URL,{method:'POST',mode:'no-cors',headers:{'Content-Type':'text/plain'},body:JSON.stringify(data)});
+await fetch(SCRIPT_URL,{method:'POST',mode:'no-cors',headers:{'Content-Type':'text/plain'},body:JSON.stringify(data)});
 alert('Đã gửi thành công!');
 e.target.reset();
 }catch(err){alert('Lỗi gửi dữ liệu');}
