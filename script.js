587fd7b892ff343888d02316c0e7d303a2201b0c
nonbestseovn-jpg <nonbestseo.vn@gmail.com>
2026-07-09 08:51:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,37 @@
 const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzt9flnRru-DxvIxDzk6qWutZuEVj7F978tmoOJs30cO-ZPnrygVDSDB84tw79dFqU/exec";
 
-const form = document.getElementById("f");
-
-form.addEventListener("submit", async function (e) {
+document.getElementById("f").addEventListener("submit", async function(e){
 
     e.preventDefault();
 
     const data = {
-        name: document.getElementById("name").value.trim(),
-        phone: document.getElementById("phone").value.trim(),
-        website: document.getElementById("website").value.trim(),
-        service: document.getElementById("service").value.trim(),
-        message: document.getElementById("message").value.trim()
+        name:name.value,
+        phone:phone.value,
+        website:website.value,
+        service:service.value,
+        message:message.value
     };
 
-    try {
+    try{
 
-        const res = await fetch(SCRIPT_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
+        await fetch(SCRIPT_URL,{
+            method:"POST",
+            mode:"no-cors",
+            headers:{
+                "Content-Type":"text/plain"
             },
-            body: JSON.stringify(data)
+            body:JSON.stringify(data)
         });
 
-        const result = await res.json();
+        alert("Đã gửi thành công");
 
-        if(result.success){
+        this.reset();
 
-            alert("Đăng ký thành công!");
-
-            form.reset();
-
-        }else{
-
-            alert(result.error);
-
-        }
-
-    } catch (err) {
+    }catch(err){
 
         console.log(err);
 
-        alert("Không kết nối được Apps Script");
+        alert("Không gửi được");
 
     }
 
